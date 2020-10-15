9080ce97ed646bbd4980a92786866079a85bd083
Laura Daugherty <lauradaugherty6@gmail.com>
2020-10-15 11:28:15 -0700

    adding build folder for deploy

---
+++ url-shortening-api-master/build/precache-manifest.d4a4432db973dd2d498675093c1dfe51.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3817f9674123a171d90beeddf1505a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de27dd9d87a6cb88984d",
+    "url": "/static/css/main.e0795f81.chunk.css"
+  },
+  {
+    "revision": "f2ba306f7ac03f0bb88a",
+    "url": "/static/js/2.a2006739.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2006739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de27dd9d87a6cb88984d",
+    "url": "/static/js/main.734a9d3f.chunk.js"
+  },
+  {
+    "revision": "d53a4290037e3821af15",
+    "url": "/static/js/runtime-main.7ef034e5.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "47ef0c281d9e90c932dfd63faa38b94f",
+    "url": "/static/media/bg-boost-mobile.47ef0c28.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "1922b68a4d1dce29b2c40904cdbc1bba",
+    "url": "/static/media/icon-brand-recognition.1922b68a.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "5029d0493c803aac6c96e13be419ecb7",
+    "url": "/static/media/illustration-working.5029d049.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/static/media/logo.6387840f.svg"
+  }
+]);
